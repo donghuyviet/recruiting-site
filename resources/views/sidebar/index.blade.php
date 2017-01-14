7fee7829a836fb6c19da668b54f7d8755ac676a8
vietdh <vietdh@flix.vn>
2017-01-14 17:49:48 +0700

    update sidebar

--- resources/views/sidebar/index.blade.php
+++ resources/views/sidebar/index.blade.php
@@ -4,8 +4,8 @@
                 </div>
                 <div class="box-content">
                     <ul>
-                        <li><a href="/orderer">List Orderer</a></li>
                         <li><a href="/orderer/entry">Create Orderer</a></li>
+                        <li><a href="/orderer">List Orderer</a></li>
                         
                     </ul>
                 </div>
