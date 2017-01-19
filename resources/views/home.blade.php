874523831024d4f9795c69062ede9e7d1786f6ff
Haleluak <huuduc93@gmail.com>
2017-01-19 17:38:42 +0700

    code page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,18 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="col-sm-6 text-center" style="background: rgba(231, 76, 60, 0.9);">
+                         <div class="content-sub">
+                                <h1>仕事を探す</h1>
+                               <a href = "{{ url('/jobs') }}"><button type="button" class="btn btn-info">仕事のリストを見る。</button></a>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 text-center" style="background: rgba(44, 62, 80, 0.9);">
+                        <div class="content-sub">
+                                <h1>仕事を依頼する。</h1>
+                                <a href = "{{ url('/seeker/entry') }}"><button type="button" class="btn btn-info">新しい仕事を依頼する。</button></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
