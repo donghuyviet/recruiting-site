9c8d7880e815a96da51cf5f0cd1e2b7d200cf6a0
Haleluak <huuduc93@gmail.com>
2017-03-16 18:08:20 +0700

    add database

---
+++ database/migrations/2017_03_16_103008_create_work_day_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkDayTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_day', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('job_id'); 
+            $table->text('number_day');
+            $table->text('time_work');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_day');
+    }
+}
